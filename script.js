67f31f6dc7b4622690801d667786af523f70863c
Marina Aleksic <almarina91@gmail.com>
2020-01-24 17:28:02 +0100

    updates
    buttons, cards, search in navbar

---
+++ script.js
@@ -0,0 +1,17 @@
+// Open the full screen search box 
+function openSearch() {
+    document.getElementById("myOverlay").style.display = "block";
+  }
+  
+  // Close the full screen search box 
+  function closeSearch() {
+    document.getElementById("myOverlay").style.display = "none";
+  }
+
+  function sendMessage() {
+    document.getElementById("myOverlayMessage").style.display = "block";
+  }
+
+  function closeMessage() {
+    document.getElementById("myOverlayMessage").style.display = "none";
+  }
